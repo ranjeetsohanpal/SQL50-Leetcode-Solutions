0edefd9d6705d0520b90c000ada38b57b9f8e58d
R. Singh <98720947+ranjeetsohanpal@users.noreply.github.com>
2024-12-04 21:35:59 +0530

    Create consecutive_numbers.sql

---
+++ consecutive_numbers.sql
@@ -0,0 +1,10 @@
+WITH lags AS (
+    SELECT num,
+    LAG(num) OVER (ORDER BY id) AS lag_1,
+    LAG(num,2) OVER (ORDER BY id) AS lag_2
+    FROM Logs
+)
+
+SELECT DISTINCT num AS ConsecutiveNums FROM lags
+WHERE num = lag_1 AND num = lag_2;
+
